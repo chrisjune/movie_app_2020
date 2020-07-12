387e9950369cee70f06195142ba97cca873601bc
chrisjune <chrisjune@gmail.com>
2020-07-12 19:52:09 +0900

    03 component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
+function Food({fav, color}){
+  return (
+    <h3>I love {fav} {color}</h3>
+  );
+}
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" color="red"/>
+    </div>
+  );
 }
 
 export default App;

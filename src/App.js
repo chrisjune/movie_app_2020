f10e513a16a696932d62dc466d0d2bb51c91b9ba
chrisjune <chrisjune@gmail.com>
2020-07-18 23:49:55 +0900

    04-3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React from 'react';
-function Food({fav, color}){
+function Food({name, picture}){
   return (
-    <h3>I love {fav} {color}</h3>
+    <div>
+      <h3>I love {name}</h3>
+      <img src={picture} width={300} alt={name}/>
+    </div>
   );
 }
+
+const foodILike = [ 
+  { 
+  'id':1,
+  'name':'kimchi',
+  'image': 'https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg'
+  },
+  {
+    'id':2,
+    'name':'samgyeopsal',
+    'image': 'https://i2.wp.com/www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg?resize=750%2C600&ssl=1'
+  },
+  {
+    'id':3,
+    'name': 'bibimbap',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/1200px-Dolsot-bibimbap.jpg'
+  },
+  {
+    'id':4,
+    'name': 'donkatsu',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/6/62/Tonkatsu_by_ayustety_in_Tokyo.jpg'
+  },
+  { 
+    'id':5,
+    'name': 'kimbap',
+    'image': 'https://media-cdn.tripadvisor.com/media/photo-s/0a/b5/bb/08/tuna-kimbap-starter.jpg'
+  }
+];
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" color="red"/>
+      {
+        foodILike.map(dish=> (<Food key={dish.id} name={dish.name} picture={dish.image}/>))
+      }
     </div>
   );
 }

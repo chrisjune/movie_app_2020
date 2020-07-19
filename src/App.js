fd7ca1e76c44c568e253f6adeba4a83d5e308edf
chrisjune <chrisjune@gmail.com>
2020-07-19 22:20:12 +0900

    component life cycle management

--- src/App.js
+++ src/App.js
@@ -1,61 +1,48 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-function Food({name, picture, rating}){
-  return (
-    <div>
-      <h3>I love {name}</h3>
-      <h4>{rating}/5</h4>
-      <img src={picture} width={300} alt={name}/>
-    </div>
-  );
-}
 
-const foodILike = [ 
-  { 
-  'id':1,
-  'name':'kimchi',
-  'image': 'https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg',
-  'rating': 3,
-  },
-  {
-    'id':2,
-    'name':'samgyeopsal',
-    'image': 'https://i2.wp.com/www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg?resize=750%2C600&ssl=1',
-    'rating': 1,
-  },
-  {
-    'id':3,
-    'name': 'bibimbap',
-    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/1200px-Dolsot-bibimbap.jpg',
-    'rating': 1,
-  },
-  {
-    'id':4,
-    'name': 'donkatsu',
-    'image': 'https://upload.wikimedia.org/wikipedia/commons/6/62/Tonkatsu_by_ayustety_in_Tokyo.jpg',
-    'rating': 1,
-  },
-  { 
-    'id':5,
-    'name': 'kimbap',
-    'image': 'https://media-cdn.tripadvisor.com/media/photo-s/0a/b5/bb/08/tuna-kimbap-starter.jpg',
-    'rating': 1,
+class App extends React.Component{
+  state = {
+    'count': 0
   }
-];
+  constructor(props){
+    super(props);
+    console.log('hello');
+  }
+  render() {
+    console.log('render');
+    return (<div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
 
-function App() {
-  return (
-    <div>
-      {
-        foodILike.map(dish=> (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>))
-      }
-    </div>
-  );
-}
+      </div>)
+  }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+  componentDidMount(){
+    console.log('mount');
+  }
+
+  componentDidUpdate(){
+    console.log('update');
+  }
+
+  componentWillUnmount(){
+    console.log('die');
+  }
+
+  add = () => {
+    console.log('add');
+    this.setState(current=>({
+      count:current.count + 1,
+
+    }))
+  }
+  minus = () => {
+    console.log('minus');
+    this.setState(current=>({
+      count:current.count - 1,
+
+    }))
+  }
 }
 export default App;

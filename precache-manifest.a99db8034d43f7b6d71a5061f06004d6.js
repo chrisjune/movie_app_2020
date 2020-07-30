a7acb0c150294a2ec033193de13cfd293f029414
chrisjune <chrisjune@gmail.com>
2020-07-31 06:41:10 +0900

    Updates

---
+++ precache-manifest.a99db8034d43f7b6d71a5061f06004d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84dd9541a721a37c3d5166a008ffd230",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c238fb6ce515f6829b04",
+    "url": "/movie_app_2020/static/css/main.968df9a3.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c238fb6ce515f6829b04",
+    "url": "/movie_app_2020/static/js/main.53e75c00.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
